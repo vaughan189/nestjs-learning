e7751cafcb132897fe9031fb742b85e85c71acbf
vaughan189 <vaughan188@gmail.com>
2020-11-30 17:02:09 +0530

    NestJS: Overview => Providers section completed

--- my-first-nestjs-app/src/cats/cats.controller.ts
+++ my-first-nestjs-app/src/cats/cats.controller.ts
@@ -1,17 +1,21 @@
 
-import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
-import { CreateCatDto, UpdateCatDto, ListAllEntities } from '../dto';
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CreateCatDto, UpdateCatDto } from './dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  create(@Body() createCatDto: CreateCatDto): string {
-    return 'This action adds a new cat';
+  async create(@Body() createCatDto: CreateCatDto): Promise<any> {
+    this.catsService.create(createCatDto);
   }
 
   @Get()
-  findAll(@Query() query: ListAllEntities): string {
-    return `This action returns all cats (limit: ${query.limit} items)`;
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
   @Get(':id')
