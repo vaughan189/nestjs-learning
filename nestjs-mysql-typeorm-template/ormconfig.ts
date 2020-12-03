73b880a772fec7950500f9c5f0836908d7df9aae
vaughan189 <vaughan188@gmail.com>
2020-12-03 15:42:00 +0530

    created a todo app in nestjs using TypeORM and MySQL

---
+++ nestjs-mysql-typeorm-template/ormconfig.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+
+module.exports = {
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  logging: true,
+  entities: [process.env.TYPEORM_ENTITIES],
+  cli: {
+    entitiesDir: process.env.TYPEORM_ENTITIES_DIR,
+  },
+  migrationsRun: true,
+};
---
+++ nestjs-mysql-typeorm-template/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotesModule } from './notes/notes.module';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({ autoLoadEntities: true }), NotesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
